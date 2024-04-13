7b5f42d2520f5039253dc75f66cd841b63fdafbd
LenovoLeo <lenovoleo506@gmail.com>
2024-04-13 22:38:39 +0500

    Did filmPage

--- kinopoisk/src/pages/FilmPage.js
+++ kinopoisk/src/pages/FilmPage.js
@@ -19,13 +19,40 @@ const FilmPage = () => {
   }, [id]);
 
   if (!film) {
-    return <div>Loading...</div>;
+    return (
+      <div className="justify-content-center d-flex fs-1 m-5 pt-5">
+        Loading...
+      </div>
+    );
   }
 
   return (
-    <div className="movie">
-      <h3>{film.title}</h3>
-      <p>{film.overview}</p>
+    <div className="film__movie container mt-5 d-flex">
+      <div className="film__poster">
+        <img
+          className="film__poster__img"
+          src={`https://image.tmdb.org/t/p/w500${film.poster_path}`}
+          alt={film.title}
+        ></img>
+      </div>
+      <div className="film__info">
+        <h4>Title: {film.original_title}</h4>
+        <p>
+          Language: <span>{film.original_language}</span>
+        </p>
+        <p>
+          Release date:<span> {film.release_date}</span>
+        </p>
+        <p>
+          Rating: <span>{film.vote_average}</span>
+        </p>
+        <p>
+          Popularity: <span>{film.popularity}</span>
+        </p>
+        <p>
+          Overview: <span>{film.overview}</span>
+        </p>
+      </div>
     </div>
   );
 };
